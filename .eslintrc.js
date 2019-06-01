62d371f89ae8d6e8ec1db6f6c879a8eaefaac333
TheNech <savchenkov.vk@yandex.ru>
2019-06-01 17:42:23 +0300

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+        'browser': true,
+        'commonjs': true,
+        'es6': true
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        'ecmaVersion': 2017
+    },
+    rules: {
+        'semi': error,
+        'quotes': ['error', 'single', {'allowTemplateLiterals': true}],
+        'indent': ['error', 4],
+        'max-len': ['error', {'code': 80, 'tabWidth': 4}],
+        'camelcase': 'error',
+        'coma-style': ['error', 'last'],
+        'no-unused-vars': 'error',
+        'array-bracket-spacing': ['error', 'never'],
+        'eqeqeq': ['error', 'always'],
+        'no-console': ['error', {allow: ['info', 'error', 'warn']}],
+        'max-params': ['error', 3],
+        'no-magic-numbers': 'error',
+        'no-multi-spaces': ['error', {ignoreEOLComments: true}],
+        'max-depth': ['error', 3]
+    }
+};
