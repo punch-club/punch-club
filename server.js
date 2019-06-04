191fcfa3829594c015a2720521e0e650a89859d5
Amal Muslim <muslim.amal@yandex.ru>
2019-06-04 16:55:53 +0300

    add handlebar

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const hbs = require("hbs");
 const port = 3000;
 
-app.use('/', express.static(__dirname + '/src/'));
 
-app.get('/', (req, res) => res.sendFile('index.html'));
+app.set("view engine", "hbs");
+app.set("views", "src/views/");
+hbs.registerPartials(__dirname + "/src/views/partials/header");
+hbs.registerPartials(__dirname + "/src/views/partials/footer");
+app.use(express.static(__dirname  + '/src/views/partials'));
+
+app.use("/", function(request, response){
+    response.render("index");   
+});
 
 app.listen(port, 
     () => console.log(`Punch club app listening on port ${port}!`)
