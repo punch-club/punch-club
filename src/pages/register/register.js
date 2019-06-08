528a0615d30e53d3040fe72ef0c09a8af3b14ce3
Egor <gladunov.e.i.1.17@gmail.com>
2019-06-08 13:45:40 +0300

    fix bug with package-lock.json

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -1,31 +1,26 @@
 // TODO обработчик сабмит на форму, режим тру-фолс
 var registerForm = document.querySelector('.register__form');
 registerForm.addEventListener('submit', function(event){
-    event.preventDefault()
+    event.preventDefault();
     var login = document.querySelector('.login');
     var password = document.querySelector('.password');
 
-    var url = 'http://localhost:3333/'
+    var url = 'http://localhost:3333/';
     if (register === true){
         url = url + 'login';
     } else {
         url = url + 'register';
-    };
-    console.log(login.value);
-    console.log(password.value)
+    }
     fetch(url, {
         method: 'POST',
         headers: {  
-            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"  
-          },  
-          body: 'username='+ login.value + '&password=' + password.value
-        })
-    .then(function(response){
-        return response.json();
+            'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'  
+        },  
+        body: 'username='+ login.value + '&password=' + password.value
     })
-    .then(function(data){
-        console.log(data);
-    })
+.then(function(response){
+    return response.json();
+})
 });
 
 var register = false;
@@ -33,13 +28,19 @@ var registerMode = document.querySelector('.register__SignInMode');
 registerMode.addEventListener('click', function(){
     register = !register;
     if (register === true){   
-        document.querySelector('.register__AccQuestion').textContent = 'Нет аккаунта?';
-        document.querySelector('.register__SignInMode').textContent = 'Зарегистрироваться';
-        document.querySelector('.register__button').textContent = 'Войти';
-    };
+        document.querySelector('.register__AccQuestion')
+        .textContent = 'Нет аккаунта?';
+        document.querySelector('.register__SignInMode')
+        .textContent = 'Зарегистрироваться';
+        document.querySelector('.register__button')
+        .textContent = 'Войти';
+    }
     if (register === false) {
-        document.querySelector('.register__AccQuestion').textContent = 'Есть аккаунт?';
-        document.querySelector('.register__SignInMode').textContent = 'Авторизация';
-        document.querySelector('.register__button').textContent = 'Зарегистрироваться';
-    };
+        document.querySelector('.register__AccQuestion')
+        .textContent = 'Есть аккаунт?';
+        document.querySelector('.register__SignInMode')
+        .textContent = 'Авторизация';
+        document.querySelector('.register__button')
+        .textContent = 'Зарегистрироваться';
+    }
 });
