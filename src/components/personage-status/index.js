efc208de3c18e10818934efbbdfe86b813a65783
ICE <ice8scream@gmail.com>
2019-06-11 00:38:24 +0300

    personage template

---
+++ src/components/personage-status/index.js
@@ -0,0 +1,14 @@
+'use strict';
+class PersonageStatus extends HTMLElement {
+    connectedCallback() {
+        const thisDocument = document.currentScript.ownerDocument;
+        console.log(document, document.currentScript);
+        console.log(document, document.currentScript);
+        const shadowRootEl = this.attachShadow({mode: 'open'});
+
+        const template = thisDocument.querySelector('#tmpl-personage-status');
+        shadowRootEl.appendChild(template.content.cloneNode(true));
+    }
+};
+
+customElements.define('personage-status', PersonageStatus);
