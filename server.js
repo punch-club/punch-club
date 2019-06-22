439382272c6513769fdef47a129d0705b43fd856
TheNech <savchenkov.vk@yandex.ru>
2019-06-22 17:42:43 +0300

    fix code for lint

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const port = 3000;
 // eslint-disable-next-line
 app.use('/', express.static(__dirname + '/src/'));
 
+// eslint-disable-next-line
 app.get('/', (req, res) => res.sendFile(__dirname + '/src/pages/register/register.html'));
 
+// eslint-disable-next-line
 app.get('/lobby', (req, res) => res.sendFile(__dirname + '/src/pages/lobby/lobby.html'));
 
 app.listen(port,
