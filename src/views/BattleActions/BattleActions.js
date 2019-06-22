7bb1cab8e80d4db5daf1225e3ff37a89cf20be79
Egor <gladunov.e.i.1.17@gmail.com>
2019-06-22 15:09:37 +0300

    работает получение значения радиокнопки

--- src/views/BattleActions/BattleActions.js
+++ src/views/BattleActions/BattleActions.js
@@ -1,4 +1,19 @@
 var turnBtn = document.querySelector('.actions__form');
+
+function getRadioValue(radioboxGroupName)
+{
+    var group = document.getElementsByName(radioboxGroupName);
+    var x = 0;
+    for (x; x < group.length; x++)
+    {
+        if (group[x].checked)
+        {
+            return (group[x].value);
+        }
+    }
+    return (false);
+}
+
 turnBtn.addEventListener('submit', function(event){
     event.preventDefault();
 });
