eaae926e2dd1b02f6daaee676c1219ba7805e07c
TheNech <savchenkov.vk@yandex.ru>
2019-06-22 17:23:06 +0300

    сохранение данных о пользователе  localStorage, добавлены роуты

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const port = 3000;
 // eslint-disable-next-line
 app.use('/', express.static(__dirname + '/src/'));
 
-app.get('/', (req, res) => res.sendFile('index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/src/pages/register/register.html'));
+
+app.get('/lobby', (req, res) => res.sendFile(__dirname + '/src/pages/lobby/lobby.html'));
 
 app.listen(port,
     () => console.info(`Punch club app listening on port ${port}!`)
